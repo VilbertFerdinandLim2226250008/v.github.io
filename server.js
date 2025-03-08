d1359d6983553e6980f585ef8807eb97d3ba7288
VilbertFerdinandLim2226250008 <145428337+VilbertFerdinandLim2226250008@users.noreply.github.com>
2025-03-08 18:19:52 +0700

    First commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+
+// Ganti dengan connection string MongoDB Anda
+const uri = 'mongodb+srv://admin:TokoSinarUtamaJAYAJAYAJAYA@tokosinarutama.yypt0.mongodb.net/?retryWrites=true&w=majority&appName=TokoSinarUtama"';
+
+const app = express();
+const client = new MongoClient(uri);
+
+app.use(cors({ origin: '*' }));  // Allow all origins for CORS
+app.use(express.json());  // Untuk membaca JSON dari request body
+
+// Route untuk menangani GET request di root ("/")
+app.get('/', (req, res) => {
+    res.send('Server Express sedang berjalan!');
+});
+
+// Endpoint untuk menambahkan user
+app.post('/api/users', async (req, res) => {
+    const { username, password, role } = req.body;
+
+    try {
+        await client.connect();
+        const db = client.db('Company');
+        const collection = db.collection('Akun');
+
+        // Simpan data user ke database
+        const result = await collection.insertOne({
+            username,
+            password,
+            role
+        });
+
+        res.status(201).json({ message: 'User berhasil ditambahkan!', userId: result.insertedId });
+    } catch (err) {
+        console.error(err.stack);
+        res.status(500).json({ message: 'Gagal menambahkan user.' });
+    } finally {
+        await client.close();
+    }
+});
+
+// Jalankan server di port 3000
+app.listen(3000, () => {
+    console.log('Server berjalan di http://localhost:3000');
+});
